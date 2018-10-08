7ae10b4cab25f695a54fc39b6a4ef275c3cdc377
Serhat Ataman <serhat.ataman@sysco.no>
2018-10-08 15:52:18 +0200

    secondary commit

--- src/main/java/com/example/demo/producer/Producer.java
+++ src/main/java/com/example/demo/producer/Producer.java
@@ -14,6 +14,7 @@ public class Producer implements Runnable{
             /* URL of the JMS server. DEFAULT_BROKER_URL means that JMS server is on localhost. */
             final String URL = ActiveMQConnection.DEFAULT_BROKER_URL;
 
+            // Getting JMS connection from the server and starting it
             ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(URL);
 
             // Creating connection from ConnectionFactory
@@ -21,15 +22,23 @@ public class Producer implements Runnable{
 
             //Starting Connection
             connection.start();
+            // JMS messages are sent and received using a Session. We will
+            // create here a non-transactional session object. If you want
+            // to use transactions you should set the first parameter to 'true'
 
             //Creating session from Connection
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-            //Creating the Queue
+            // Destination represents here our queue 'ThisIsQueueName' on the
+            // JMS server. You don't have to do anything special on the
+            // server to create it, it will be created automatically.
             Destination destination = session.createQueue("ThisIsQueueName");
+            // You can create any/many queue names as per your requirement.
 
             //Creating a Producer
             MessageProducer producer = session.createProducer(destination);
+            // MessageProducer is used for sending messages (as opposed
+            // to MessageConsumer which is used for receiving them)
 
             //Creating a text message
             TextMessage textMessage = session.createTextMessage("This is message number " + ++counter + "." );
